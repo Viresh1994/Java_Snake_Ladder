976fb179b5dc3331c743942142bf298ac023d9a9
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-19 08:28:48 +0530

    ADDED: Code for die to get num between 1 to 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int start_Position = 0;
         System.out.println("Wel come to Snake Ladder Problem!");
+        int dice = (int) Math.floor(Math.random()*6+1);
+        System.out.println(dice);
     }
 }
