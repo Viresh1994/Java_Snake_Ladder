12a0493905d716dc89f978c05c5d9215eaf44948
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-19 08:24:50 +0530

    ADDED: Player Start Position 0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int start_Position = 0;
         System.out.println("Wel come to Snake Ladder Problem!");
     }
 }
